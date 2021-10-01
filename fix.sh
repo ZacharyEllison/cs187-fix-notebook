6670da180173b693a008733078a9155fd6d82030
zacharyellison <zellison@college.harvard.edu>
2021-10-01 00:49:48 -0400

    Fix lab2-1 bug, made more friendly and logical in layout, centralized in main()

--- fix.sh
+++ fix.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 fix_file=${1:-fubar.ipynb}
+run_fix=${2:-False}
 
+## Revert command ##
+function fix {
+  cp "$fix_file.bak" $fix_file
+}
+
+## Makes the file pretty for replace_commands ##
+function pretty_file {
+  # pretty-print to new file
+  # same file would write as it reads (?!)
+  python -mjson.tool $fix_file > "$fix_file.new" && \
+  mv "$fix_file.new" $fix_file
+}
+
+## Finds and Replaces commands ##
 function replace_commands {
   find_string='\\newcommand{\\'
-  if [[ $fix_file == "fubar.ipynb" ]]; then
-    echo "Usage: bash fix-compatible-katex.sh <file-name>"
-    return 0
-  fi
-
-  echo "backup $fix_file in case of corruption"
-  find . -name "$(echo $fix_file)" -exec cp "-n" "{}" "{}.bak" \;
-
   echo "Finding $find_string" && echo
 
   # find the new command lines in the notebook
@@ -46,4 +53,26 @@ function replace_commands {
   done
 }
 
-replace_commands
+## Holds management logic ##
+function main {
+  if [[ $fix_file == "fubar.ipynb" ]]; then
+    echo "Usage: bash fix.sh <file-name>"
+    echo "To revert to original: bash fix.sh <file-name> revert"
+    return -1
+  fi
+
+  if [[ $run_fix == "revert" ]]; then
+    echo Running $run_fix ...
+    fix;
+    return 0
+  fi
+
+  echo "backup $fix_file in case of corruption"
+  find . -name "$(echo $fix_file)" -exec cp "-n" "{}" "{}.bak" \;
+
+  pretty_file && \
+  replace_commands
+  return 0
+}
+
+main
